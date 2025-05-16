d7835ca6f19894343485af89a439b7c43a28d0c6
mistervb <dragonxdgames@gmail.com>
2025-05-15 22:06:33 -0300

    feat(core-module): Adicionei um interceptador de requisições http, criei o configurador global do mvc, adicionei comentários

---
+++ src/main/java/br/com/blackhunter/hunter_wallet/rest_api/config/WebConfig.java
@@ -0,0 +1,43 @@
+/**
+ * 2025 © Black Hunter - Todos os Direitos Reservados.
+ *
+ * Classe protegida - Alterações somente por CODEOWNERS.
+ */
+
+package br.com.blackhunter.hunter_wallet.rest_api.config;
+
+import br.com.blackhunter.hunter_wallet.rest_api.config.inteceptor.HttpContextInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Classe <code>WebConfig</code>.
+ * <p>Configuração do Spring MVC para a aplicação Hunter Wallet REST API.</p>
+ * <p>Implementa a interface {@link WebMvcConfigurer} para personalizar a configuração do Spring MVC.</p>
+ * <p>Responsável por registrar interceptadores e outras configurações relacionadas à web.</p>
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final HttpContextInterceptor httpContextInterceptor;
+
+    /**
+     * Construtor da classe.
+     * @param httpContextInterceptor Interceptador de contexto HTTP a ser registrado
+     * <p>Injeta as dependências necessárias para a configuração do Spring MVC.</p>
+     */
+    public WebConfig(
+        HttpContextInterceptor httpContextInterceptor
+    ) {
+        this.httpContextInterceptor = httpContextInterceptor;
+    }
+
+    /**
+     * @param registry
+     * <p>Método que adiciona o inteceptador de requisições HTTP.</p>
+     * */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(httpContextInterceptor);
+    }
+}
