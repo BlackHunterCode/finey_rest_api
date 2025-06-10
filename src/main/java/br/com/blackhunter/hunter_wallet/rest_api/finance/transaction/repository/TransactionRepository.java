2c4d787ef3a17d2db71a29c03709fb7818ea47f3
mistervb <dragonxdgames@gmail.com>
2025-06-09 22:37:20 -0300

    feature(finance-module) - Copy

--- src/main/java/br/com/blackhunter/hunter_wallet/rest_api/finance/transaction/repository/TransactionRepository.java
+++ src/main/java/br/com/blackhunter/hunter_wallet/rest_api/finance/transaction/repository/TransactionRepository.java
@@ -1,3 +1,12 @@
+/*
+ * @(#)TransactionRepository.java
+ *
+ * Copyright 2025, Black Hunter
+ * http://www.blackhunter.com.br
+ *
+ * Todos os direitos reservados.
+ */
+
 package br.com.blackhunter.hunter_wallet.rest_api.finance.transaction.repository;
 
 import br.com.blackhunter.hunter_wallet.rest_api.finance.transaction.entity.TransactionEntity;
